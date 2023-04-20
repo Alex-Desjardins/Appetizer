acfe1a05eb74dc703c24d63d7c3522b18e36b8cb
Alex Desjardins <ardesjardins91@gmail.com>
2023-04-20 15:28:52 -0600

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Alex Desjardins on 4/20/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

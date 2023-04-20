6b8ad6f11663a95439973ac8c8bf8c284055502f
Alex Desjardins <ardesjardins91@gmail.com>
2023-04-20 16:09:20 -0600

    first commit

---
+++ Appetizers/AppertizerListView.swift
@@ -0,0 +1,20 @@
+//
+//  AppertizerListView.swift
+//  Appetizers
+//
+//  Created by Alex Desjardins on 4/20/23.
+//
+
+import SwiftUI
+
+struct AppertizerListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppertizerListView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppertizerListView()
+    }
+}
